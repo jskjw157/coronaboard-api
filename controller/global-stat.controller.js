e8eb716f34f3a3b87c26fc79bb3ad7929e44b35a
johny <jskjw157@gmail.com>
2024-01-06 18:24:25 +0900

    ✅ global-controller에 error handler 적용

--- controller/global-stat.controller.js
+++ controller/global-stat.controller.js
@@ -1,4 +1,5 @@
 const {GlobalStat} = require('../database');
+const { wrapWithErrorHandler } = require('../utils');
 
 async function getAll(req, res) {
     const result = await GlobalStat.findAll();
@@ -38,9 +39,9 @@ async function remove(req, res) {
     res.status(200).json({ result: 'success' });
 }
 
-module.exports = {
+module.exports = wrapWithErrorHandler({
     getAll,
     insertOrUpdate,
     remove,
-}
+});
 
